c9656831dec9a6041d14b6d04570e6e63a2e19f9
ziles84 <imamovicdamir@yahoo.com>
2021-03-10 13:22:38 +0100

    Adding ex07 files

---
+++ ex07/src/index.js
@@ -0,0 +1,25 @@
+function main() {
+
+// Variable declaration
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+
+// Variable assignment
+
+var studlyCapVar = 10;
+var properCamelCase = "A string";
+var titleCaseOver = 9000;
+
+return {
+  studlyCapVar,
+  properCamelCase,
+  titleCaseOver
+}
+
+}
+
+console.log(main());
+
+module.exports = main;
